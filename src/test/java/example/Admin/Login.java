634f3009b7be08d2211ce7f0f3b9f1719310f700
imrilei <rileipham.work@gmail.com>
2024-12-02 19:42:50 +0700

    Login and LoginPage

---
+++ src/test/java/example/Admin/Login.java
@@ -0,0 +1,18 @@
+package example.Admin;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        LoginPage loginPage = new LoginPage(driver);
+        driver.manage().window().maximize();
+
+
+        driver.get("http://14.176.232.213:8080/EBankingWebsite/faces/admin/Login.xhtml");
+
+        loginPage.login("1","admin");
+    }
+}
+
