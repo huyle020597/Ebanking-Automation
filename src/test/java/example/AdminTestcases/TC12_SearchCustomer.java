cf13d52b79fbc1cf272f042f8933d07b34a6e921
huyle020597 <huyle020597@gmail.com>
2024-12-27 20:14:04 +0700

    Last Updated 19:47 27/12

--- src/test/java/example/AdminTestcases/TC12_SearchCustomer.java
+++ src/test/java/example/AdminTestcases/TC12_SearchCustomer.java
@@ -61,10 +61,10 @@ public class TC12_SearchCustomer {
         originalHandle = driver.getWindowHandle();
     }
 
-//    @AfterMethod
-//    public void cleanUp() {
-//        driver.quit();
-//    }
+    @AfterMethod
+    public void cleanUp() {
+        driver.quit();
+    }
 
     @Test
             (description = "Admin - Search customer by multiple fields")
